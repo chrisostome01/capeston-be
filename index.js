09a475044804f98f062c8b46cf43759a86e792f6
unknown <sezeranochrisostom123@gmail.com>
2022-01-21 02:03:06 +0200

    Changing server

--- index.js
+++ index.js
@@ -22,8 +22,8 @@ const options = {
            version: "1.0.1"
        },
        servers:[{
-         url: `http://localhost:${PORT}`,
-         surl: `https://capstonetyu.herokuapp.com/`
+         surl: `http://localhost:${PORT}`,
+         url: `https://capstonetyu.herokuapp.com/`
        }],      
    },
    apis: ["./routes/*.js"]
